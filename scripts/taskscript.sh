4b5fb1a53e099f51c0a341b54e2f2ec23fc54547
Marcos Tijerina <Marcos.Tijerina@thisismap.com>
2019-10-22 11:52:55 -0500

    removing LF

--- scripts/taskscript.sh
+++ scripts/taskscript.sh
@@ -2,13 +2,16 @@
 
 sleep 3
 
-systemctl status sendmail
-systemctl status saslauthd
+
+
      # Use the following to test, BUT I recommend remove it before upload to ECR or Docker Hub
      # exec printf 'Subject: Test\nFrom: your.name@emailtest.com\nTo: your.name@emailtest.com\n' | sendmail -i -t -f your.name@emailtest.com your.name@emailtest.com
 
 ## You can use this as a system halt iF you are running task
 ##NOTE: To use halt, you need to have /sys/fs/cgroup readonly and /run tmpfs otherwise you will trigger a shutdown of the Docker Linux HOST
 #systemctl halt
+
+##This will show you details if your email fails
+systemctl status sendmail -l
 
 exit 0
