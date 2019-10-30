3ddda8740b13f920221e5eec6fa65d7a5b9e83a3
foreshorten <foreshorten.com@gmail.com>
2019-10-30 18:50:46 -0500

    remove quotes

--- scripts/emailcfg.sh
+++ scripts/emailcfg.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
      echo 127.0.0.1 localhost localhost.localdomain $(hostname) >> /etc/hosts
-     echo \"AuthInfo:$SMTP_ENDPOINT\" \"U:root\" \"I:$SENDMAIL_USER\" \"P:$SENDMAIL_PASS\" \"M:PLAIN\" > /etc/mail/authinfo
+     echo AuthInfo:$SMTP_ENDPOINT \"U:root\" \"I:$SENDMAIL_USER\" \"P:$SENDMAIL_PASS\" \"M:PLAIN\" > /etc/mail/authinfo
      sed -i -e "s/example.com/$SEND_DOMAIN/g" /etc/mail/sendmail.mc
      sed -i -e "s/email-smtp.us-west-2.amazonaws.com/$SMTP_ENDPOINT/g" /etc/mail/sendmail.mc
 sleep 2
-     systemctl start sendmail
-     systemctl start saslauthd
+systemctl start sendmail
+systemctl start saslauthd
 
-     systemctl enable sendmail
-     systemctl enable saslauthd
+systemctl enable sendmail
+systemctl enable saslauthd
 exit 0
